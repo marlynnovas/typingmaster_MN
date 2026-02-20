d1d2d2d62b149c06fc21109f3b7f9a5055daa72d
Marlyn Novas <marlynysabelnovas@gmail.com>
2026-02-20 12:58:55 -0400

    Word VOCAB

--- git_typingmaster_MN.py
+++ git_typingmaster_MN.py
@@ -5,5 +5,12 @@ def main (page: ft.Page):
     page.title= "Typing Master"
 
     words_list = [
-        
+        "otorrinolaringologo", "electroencefalograma",
+        "hipopotomonstrosesquipedaliofobia", "desoxirribonucleico",
+        "paralelepipedo", "inconstitucionalidad",
+        "esternocleidomastoideo", "anticonstitucional",
+        "electrodomestico", "circunferencia",
+        "desafortunadamente", "extraordinario",
+        "incomprensible", "psicologia",
+        "responsabilidad"
     ]
